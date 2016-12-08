8261a9e6cb6afbb7efd853afb0a2a33c05810ece
Nadia Pillard <ua9ond@yandex.ru>
2016-12-08 18:55:38 +0700

    titleHtml by title replaced

--- js/screens/result.js
+++ js/screens/result.js
@@ -5,7 +5,7 @@ import {welcome} from '../data';
 
 export default (data) => {
 
-  const titleHtml = `<h2 class="title">${data.title}</h2>`;
+  const title = `<h2 class="title">${data.title}</h2>`;
   const resultContent = `<div class="main-stat">За&nbsp;${data.time}&nbsp;минуты<br>вы&nbsp;отгадали ${data.quantity}&nbsp;мелодии</div>`;
   const comparison = `<span class="main-comparison">Это&nbsp;лучше чем у&nbsp;${data.comparison}%&nbsp;игроков`;
 
@@ -13,7 +13,7 @@ export default (data) => {
   const content =
     `<section class="main main--result">
       <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-      ${titleHtml}
+      ${title}
       ${resultContent}
       ${comparison}
       <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
